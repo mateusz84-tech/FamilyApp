bcbafc11032292753518f7689733b2b8b754c5a9
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2022-08-04 19:56:49 +0200

    Utworzenie klasy dla przechowywania danych o obiektach rodzina.

---
+++ src/main/java/pl/matkoc/FamilyApp/model/Family.java
@@ -0,0 +1,101 @@
+package pl.matkoc.FamilyApp.model;
+
+import java.util.Objects;
+
+public class Family implements Comparable<Family>{
+
+    private Integer id;
+    private String familyName;
+    private Integer nrOfAdults;
+    private Integer nrOfChildren;
+    private Integer nrOfInfants;
+
+    public Family(String familyName, Integer nrOfAdults, Integer nrOfChildren, Integer nrOfInfants) {
+        this.familyName = familyName;
+        this.nrOfAdults = nrOfAdults;
+        this.nrOfChildren = nrOfChildren;
+        this.nrOfInfants = nrOfInfants;
+    }
+
+    public Family() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Family family = (Family) o;
+        return familyName.equals(family.familyName) && nrOfAdults.equals(family.nrOfAdults)
+                && nrOfChildren.equals(family.nrOfChildren) && nrOfInfants.equals(family.nrOfInfants);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(familyName, nrOfAdults, nrOfChildren, nrOfInfants);
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFamilyName() {
+        return familyName;
+    }
+
+    public void setFamilyName(String familyName) {
+        this.familyName = familyName;
+    }
+
+    public Integer getNrOfAdults() {
+        return nrOfAdults;
+    }
+
+    public void setNrOfAdults(Integer nrOfAdults) {
+        this.nrOfAdults = nrOfAdults;
+    }
+
+    public Integer getNrOfChildren() {
+        return nrOfChildren;
+    }
+
+    public void setNrOfChildren(Integer nrOfChildren) {
+        this.nrOfChildren = nrOfChildren;
+    }
+
+    public Integer getNrOfInfants() {
+        return nrOfInfants;
+    }
+
+    public void setNrOfInfants(Integer nrOfInfants) {
+        this.nrOfInfants = nrOfInfants;
+    }
+
+    @Override
+    public String toString() {
+        return "Family{" +
+                "id=" + id +
+                ", familyName='" + familyName + '\'' +
+                ", nrOfAdults=" + nrOfAdults +
+                ", nrOfChildren=" + nrOfChildren +
+                ", nrOfInfants=" + nrOfInfants +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Family otherFamily) {
+
+        int result = this.familyName.compareToIgnoreCase(otherFamily.getFamilyName());
+        if(result == 0)
+            result = this.nrOfAdults.compareTo(otherFamily.getNrOfAdults());
+        if(result == 0)
+            result = this.nrOfChildren.compareTo(otherFamily.getNrOfChildren());
+        if(result == 0)
+            result = this.nrOfInfants.compareTo(otherFamily.getNrOfInfants());
+
+        return result;
+    }
+}
